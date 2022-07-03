2a98d7ce7b74ef39cd152811f0bc7739481a7a5f
polina993 <>
2022-07-03 16:13:04 +0300

    add task47

---
+++ task47/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int m = Input("Введите количество строк: ");
+int n = Input("Введите количество столбцов: ");
+
+double[,] numbers = new double[m, n];
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        numbers[i, j] = Convert.ToDouble(new Random().Next(-100, 1000)) / 10;
+    }
+}
+PrintArray(numbers);
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
