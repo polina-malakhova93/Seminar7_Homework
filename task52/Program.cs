0c33aa445c429cb68f97f388aca6076922cbf4a4
polina993 <>
2022-07-03 18:04:56 +0300

    add task52

---
+++ task52/Program.cs
@@ -0,0 +1,51 @@
+﻿
+int m = Input("Введите количество строк: ");
+int n = Input("Введите количество столбцов: ");
+
+int[,] numbers = new int[m, n];
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        numbers[i, j] = new Random().Next(1, 10);
+    }
+}
+PrintArray(numbers);
+
+double[] sum = new double[m];
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum[i] += numbers[j, i];
+    }
+}
+
+PrintArrayAverage(sum);
+
+void PrintArrayAverage(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]/m  + " ");
+    }
+    Console.WriteLine();
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
